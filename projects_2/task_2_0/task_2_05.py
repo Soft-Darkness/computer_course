99c514175d78614f4e335f4ea8e1c27481b6d872
Алексей <aleksvasa75@gmail.com>
2026-02-06 12:59:31 +0300

    Add files via upload

---
+++ projects_2/task_2_0/task_2_05.py
@@ -0,0 +1,4 @@
+name='Вася'
+group=int(50002)
+score=int(5)
+print(f'Студент {name} из группы {group} получил {score} баллов')
---
+++ projects_2/task_2_0/task_2_06.py
@@ -0,0 +1,3 @@
+f = open('C:/Users/АВ/Desktop/Vasiliev_AM/output.txt','w',encoding='UTF-8')
+print('Васильев Алексей Максимович. Первый курс ИБСиБ. Группа 4731204/50002', file=f)
+f.close()
