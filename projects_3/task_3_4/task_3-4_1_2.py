f43ee13db6ec62a13bc5295d008a1311c98bae4c
Алексей <aleksvasa75@gmail.com>
2026-02-26 22:07:47 +0300

    Add files via upload
    Uploaded task_3_4

---
+++ projects_3/task_3_4/task_3-4_1_2.py
@@ -0,0 +1,31 @@
+echo -e "echo -e "ls -lah / и т.д. "\\n > ./Desktop/task_3-4_1_2.py && ls -lah / >> ./Desktop/task_3-4_1_2.py 
+
+итого 8,1G
+drwxr-xr-x  23 root root 4,0K фев  8 10:00 .
+drwxr-xr-x  23 root root 4,0K фев  8 10:00 ..
+lrwxrwxrwx   1 root root    7 фев  8 09:11 bin -> usr/bin
+drwxr-xr-x   2 root root 4,0K фев 26  2024 bin.usr-is-merged
+drwxr-xr-x   4 root root 4,0K фев 12 19:06 boot
+drwxr-xr-x   2 root root 4,0K фев  8 09:14 cdrom
+drwxr-xr-x  21 root root 4,7K фев 26 20:16 dev
+drwxr-xr-x 148 root root  12K фев 12 19:06 etc
+drwxr-xr-x   3 root root 4,0K фев  8 09:14 home
+lrwxrwxrwx   1 root root    7 фев  8 09:11 lib -> usr/lib
+lrwxrwxrwx   1 root root    9 фев  8 09:11 lib64 -> usr/lib64
+drwxr-xr-x   2 root root 4,0K апр  8  2024 lib.usr-is-merged
+drwx------   2 root root  16K фев  8 09:10 lost+found
+drwxr-xr-x   4 root root 4,0K фев 13 22:16 media
+drwxr-xr-x   2 root root 4,0K янв  8 21:32 mnt
+drwxr-xr-x   3 root root 4,0K фев  8 10:44 opt
+dr-xr-xr-x 376 root root    0 фев 26  2026 proc
+drwx------   9 root root 4,0K фев  8 14:53 root
+drwxr-xr-x  35 root root  900 фев 26 20:55 run
+lrwxrwxrwx   1 root root    8 фев  8 09:11 sbin -> usr/sbin
+drwxr-xr-x   2 root root 4,0K мар 31  2024 sbin.usr-is-merged
+drwxr-xr-x   2 root root 4,0K янв  8 21:32 srv
+-rw-------   1 root root 8,0G фев 21 15:36 swapfile
+dr-xr-xr-x  13 root root    0 фев 26 20:45 sys
+drwxr-xr-x   9 root root 4,0K фев 12 19:00 timeshift
+drwxrwxrwt  18 root root 4,0K фев 26 20:55 tmp
+drwxr-xr-x  12 root root 4,0K янв  8 21:32 usr
+drwxr-xr-x  11 root root 4,0K фев  8 06:19 var
